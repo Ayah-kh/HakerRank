dce0fafc9414aeca8056662ef72a017743aa9f20
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-19 16:49:25 +0200

    First Commit

---
+++ PalindromeWord.java
@@ -0,0 +1,13 @@
+public class PalindromeWord {
+//    A palindrome is a word, phrase, number,
+//    or other sequence of characters which reads
+//    the same backward or forward.
+
+    public static void main(String[] args) {
+
+
+        String name="g";
+
+
+    }
+}
