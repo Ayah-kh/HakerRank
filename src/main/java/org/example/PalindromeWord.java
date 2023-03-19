f52d228aab60c71c0e83a2f4abfdfa92cc711de7
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-19 17:07:21 +0200

    First Commit

--- src/main/java/org/example/PalindromeWord.java
+++ src/main/java/org/example/PalindromeWord.java
@@ -1,5 +1,8 @@
 package org.example;
 
+
+import java.util.Arrays;
+
 public class PalindromeWord {
 
 //    A palindrome is a word, phrase, number,
@@ -9,7 +12,24 @@ public class PalindromeWord {
     public static void main(String[] args) {
 
 
-        String name = "g";
+        String word = "madfffam";
+        char[] words = word.toCharArray();
+        System.out.println(Arrays.toString(words));
+        System.out.println(words.length);
+        boolean isPalindrome = false;
+
+        for (int i = 0; i < words.length / 2; i++) {
+            if (words[i] == words[words.length - 1 - i])
+                isPalindrome = true;
+            else {
+                isPalindrome = false;
+
+            }
+
+            System.out.println("i: " + i + ">>" + words[i] +
+                    " - " + "Array length-1 : " + words[words.length - 1 - i]);
+            System.out.println(isPalindrome);
+        }
 
 
     }
